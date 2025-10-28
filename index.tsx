a02fcdfc416b941aee131680ed32f3f29275de71
gamifiai <lee.kw.kevin@gmail.com>
2025-10-28 17:43:31 +0800

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log("index.tsx script executed.");
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+  console.error("Fatal Error: Could not find the root element in index.html. The React app cannot be mounted.");
   throw new Error("Could not find root element to mount to");
 }
 
+console.log("Root element found. Rendering React app...");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
